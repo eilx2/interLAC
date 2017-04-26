9798d823b4fec143c79839b9bb17c3a239b88c1d
minhminh.- <haminh1998@gmail.com>
2017-04-26 22:31:13 +0200

    Add Camera and Chat Main

---
+++ proj/app/src/main/java/com/mun/minh_minhh/interlac/CameraMain.java
@@ -0,0 +1,13 @@
+package com.mun.minh_minhh.interlac;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CameraMain extends BasicActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_main);
+    }
+}
--- proj/app/src/main/java/com/mun/minh_minhh/interlac/HomePage.java
+++ proj/app/src/main/java/com/mun/minh_minhh/interlac/HomePage.java
@@ -9,12 +9,34 @@ import android.widget.Button;
 public class HomePage extends BasicActivity {
 
     public Button button;
-    public void init(){
+    public void init1(){
         button = (Button)findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent toy = new Intent(HomePage.this, EventMain.class);
+                startActivity(toy);
+            }
+        });
+
+    }
+    public void init2(){
+        button = (Button)findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toy = new Intent(HomePage.this, CameraMain.class);
+                startActivity(toy);
+            }
+        });
+
+    }
+    public void init3(){
+        button = (Button)findViewById(R.id.button3);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toy = new Intent(HomePage.this, ChatMain.class);
                 startActivity(toy);
             }
         });
@@ -26,7 +48,9 @@ public class HomePage extends BasicActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-        init();
+        init1();
+        init2();
+        init3();
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
