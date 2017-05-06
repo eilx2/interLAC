041665b65896789d1066fa40565a94113573eadd
minhminh.- <haminh1998@gmail.com>
2017-05-06 16:55:52 +0200

    Add 1st event page (example)

--- proj/app/src/main/java/com/mun/minh_minhh/interlac/EventMain.java
+++ proj/app/src/main/java/com/mun/minh_minhh/interlac/EventMain.java
@@ -6,8 +6,22 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class EventMain extends BasicActivity {
+    public Button button;
+    public void init_theater_button(){
+        button = (Button)findViewById(R.id.button4);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toy = new Intent(EventMain.this, TheaterMain.class);
+                startActivity(toy);
+            }
+        });
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +34,7 @@ public class EventMain extends BasicActivity {
         Menu menu = bottomNavigationView.getMenu();
         MenuItem menuItem = menu.getItem(1);
         menuItem.setChecked(true);
+        init_theater_button();
 
 
     }
