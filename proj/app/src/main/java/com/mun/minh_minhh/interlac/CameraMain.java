c8405928b9324d030f6667e1465ceb3ab911167a
volkll <luka@lukas-mbp.mobile.usilu.net>
2017-05-11 13:43:35 +0200

    Improved the music evt page design

--- proj/app/src/main/java/com/mun/minh_minhh/interlac/CameraMain.java
+++ proj/app/src/main/java/com/mun/minh_minhh/interlac/CameraMain.java
@@ -26,13 +26,9 @@ public class CameraMain extends BasicActivity{
 
     /** Check if this device has a camera */
     private boolean checkCameraHardware(Context context) {
-        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
-            // this device has a camera
-            return true;
-        } else {
-            // no camera on this device
-            return false;
-        }
+        // this device has a camera
+// no camera on this device
+        return context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA);
     }
 
     /** A safe way to get an instance of the Camera object. */
