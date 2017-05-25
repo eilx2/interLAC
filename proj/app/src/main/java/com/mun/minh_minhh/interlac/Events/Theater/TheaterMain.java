bdbcd18774403dcc1687cbb27ce1a75b483ee8d0
minhminh.- <haminh1998@gmail.com>
2017-05-25 16:02:29 +0200

    Improved Layout

--- proj/app/src/main/java/com/mun/minh_minhh/interlac/Events/Theater/TheaterMain.java
+++ proj/app/src/main/java/com/mun/minh_minhh/interlac/Events/Theater/TheaterMain.java
@@ -10,6 +10,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
